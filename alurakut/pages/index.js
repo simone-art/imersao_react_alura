fe472c385dcd6de91a70bcea71fa6dc63a64e0b8
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-13 18:42:30 -0300

    Criado componente Box

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,10 +1,24 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+//Constantes vao em maiúsculas porque elas são componentes não tags
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Main>
+    
+    
+    Images
+    Bemvindo
+    Comunidades
+    
+    </Main>
 }
