c155f00be8705197f168ee0ef42fa992902fb182
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-14 17:12:00 -0300

    Adicionado código pra configurar o CSS e ter responsividade

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -13,12 +13,25 @@ const Box = styled.div`
 
 //Criado componente MainGrid
 //Grid-gap para ver os cards separados
+// width: 100% para pegar a parte do mobile
+//max-width: 500px pra nao esticar tanto no celular
 const MainGrid = styled.main`
-   display: grid;
+   width: 100%;
    grid-gap: 10px;
+   margin-left: auto;
+   margin_right: auto;
+   max-width: 500px;
    padding: 16px;
+   .profileArea {
+    display: none;
+    @media(min-width: 860px){
+      display: block;
+    }
+   }
 
    @media(min-width: 860px){
+    max-width: 1110px;
+    display: grid;
     grid-template-areas: "profileArea welcomeArea profileRelationsArea"; 
     grid-template-column: 160px 1fr 312px;
    }
@@ -28,19 +41,19 @@ const MainGrid = styled.main`
 
 export default function Home() {
   return <MainGrid>
-    <div style={{ gridArea: 'profileArea' }}>
+    <div className="profileArea" style={{ gridArea: 'profileArea' }}>
       <Box>
         Images
       </Box>
     </div>
 
-    <div style={{ gridArea: 'welcomeArea' }} >
+    <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }} >
       <Box>
         Bemvindo
       </Box>
     </div>
     
-   <div style={{ gridArea: 'profileRelationsArea' }}>
+   <div  className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
     <Box>
       Pessoas da Comunidade
     </Box>
