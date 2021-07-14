1be80a98e90ad4fa20b29888fe95ff1d4f31bc3d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-14 18:11:59 -0300

    Criada pasta Box, arquivo index.js e colocado código const Box

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -5,14 +5,6 @@ import styled from 'styled-components'
 //   color: ${({ theme }) => theme.colors.primary};
 // `
 
-//Constantes vao em maiúsculas porque elas são componentes não tags
-const Box = styled.div`
-  background: #FFFFFF;
-  border-radius: 8px;
-`;
-
-
-
 export default function Home() {
   return (
    <MainGrid>
---
+++ alurakut/pages/src/components/Box/index.js
@@ -0,0 +1,5 @@
+//Constantes vao em maiúsculas porque elas são componentes não tags
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
+`;
